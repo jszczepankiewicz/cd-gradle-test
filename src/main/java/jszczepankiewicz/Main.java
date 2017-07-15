ec143a8ae492cf1fd7b431a2d1c1e315b7b67c7a
jszczepankiewicz <jszczepankiewicz@gmail.com>
2017-07-15 19:06:17 +0200

    initial version

---
+++ src/main/java/jszczepankiewicz/Main.java
@@ -0,0 +1,12 @@
+package jszczepankiewicz;
+
+/**
+ * @author jszczepankiewicz
+ * @since 2017-07-11
+ */
+public class Main {
+
+  public static void main(String...args){
+    System.out.println("italo disco rulez!");
+  }
+}
