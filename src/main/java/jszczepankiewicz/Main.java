47ac5ca0a4fa0bd4ec115ed315aa1798291cc997
jszczepankiewicz <jszczepankiewicz@gmail.com>
2017-07-24 00:01:30 +0200

    Separated whole ci to external file. Separate integration tests added as separate task extending tests and being dependency to check task. Most likely requires Gradle 4.

--- src/main/java/jszczepankiewicz/Main.java
+++ src/main/java/jszczepankiewicz/Main.java
@@ -1,6 +1,7 @@
 package jszczepankiewicz;
 
 /**
+ * Some comment.
  * @author jszczepankiewicz
  * @since 2017-07-11
  */
@@ -9,6 +10,8 @@ public class Main {
   public int whatsTheNumber(){
     return 3;
   }
+
+  public int whatsTheNumberForIntegrationTest(){ return 5;};
 
   public static void main(String...args){
     System.out.println("italo disco rulez!@");
